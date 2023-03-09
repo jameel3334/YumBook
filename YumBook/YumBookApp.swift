e8aaef4fe891379b5ccd1b374a4d4191e89c5146
Mohammed Jameeluddin <jameel3334@gmail.com>
2023-03-09 11:55:29 -0500

    Initial Commit

---
+++ YumBook/YumBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumBookApp.swift
+//  YumBook
+//
+//  Created by Mohammed Jameeluddin on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct YumBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
